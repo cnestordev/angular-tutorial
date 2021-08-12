30a34e1243663c55846ed5867cf0813d0d45ff83
Nestor <ncervan87@gmail.com>
2021-08-12 13:00:55 -0700

    first commit, users component

---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { IUser } from '../../models/User';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.sass'],
+})
+export class UserComponent implements OnInit {
+  user!: IUser;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.user = {
+      firstName: 'John',
+      lastName: 'Doe',
+      age: 20,
+      address: {
+        street: '123 Fake St',
+        city: 'Boston',
+        state: 'MA',
+      },
+    };
+  }
+}
---
+++ src/app/components/users/users.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+
+import { IUser } from 'src/app/models/User';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.sass'],
+})
+export class UsersComponent implements OnInit {
+  users!: IUser[];
+  showExtended: boolean = true;
+  loaded: boolean = false;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.users = [
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        age: 20,
+        address: {
+          street: '123 Fake St',
+          city: 'Boston',
+          state: 'MA',
+        },
+      },
+      {
+        firstName: 'Jane',
+        lastName: 'Dane',
+        age: 22,
+        address: {
+          street: '242 Not Real St',
+          city: 'Dallas',
+          state: 'TX',
+        },
+      },
+      {
+        firstName: 'Steve',
+        lastName: 'Stevenson',
+        age: 30,
+        address: {
+          street: '55 Real St',
+          city: 'Miami',
+          state: 'FL',
+        },
+      },
+    ];
+
+    this.addUser({
+      firstName: 'Test',
+      lastName: 'TESER',
+    });
+
+    this.loaded = true;
+  }
+
+  addUser(user: IUser) {
+    this.users.push(user);
+  }
+}
